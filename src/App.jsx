fcd3b553647c0a959caee8c866bdbcbe5b9aa158
Jason <jfadelli@gmail.com>
2025-06-07 00:27:39 -0700

    win 10 commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,12 +23,17 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </div>
+      <div className="card">
+        <button
+          onClick={() => console.log('hello')}
+        >
+          Login
+        </button>
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more!
+        Click on the Vite and React logos to learn more!!!
       </p>
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: true, // equivalent to 0.0.0.0
-    port: 5173,
+    host: '0.0.0.0',
+    port: 5173, // or your port
+    strictPort: true,
+    watch: {
+      usePolling: true, // for WSL2 or Docker filesystem issues
+    },
   },
 })
